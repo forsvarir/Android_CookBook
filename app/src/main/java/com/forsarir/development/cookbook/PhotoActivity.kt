f6bb813c3e6468623158c74dcb7675be05270303
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-05-09 00:51:52 +0100

    Take a photo and show the thumbnail

--- app/src/main/java/com/forsarir/development/cookbook/PhotoActivity.kt
+++ app/src/main/java/com/forsarir/development/cookbook/PhotoActivity.kt
@@ -1,11 +1,20 @@
 package com.forsarir.development.cookbook
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 
 class PhotoActivity : AppCompatActivity() {
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var snapImage: ImageView
+
+    // TODO: Need to save/restore imageview on for example phone rotation
+    // TODO: Consider how to return to previous activity, rather than starting home activity over again
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
@@ -15,6 +24,25 @@ class PhotoActivity : AppCompatActivity() {
         homeButton.setOnClickListener {
             val homeIntent = Intent(this, MainActivity::class.java).apply {}
             startActivity(homeIntent)
+        }
+
+        val snapButton: Button = findViewById(R.id.buttonSnap)
+
+        snapButton.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+
+        snapImage = findViewById(R.id.imageSnap)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            snapImage.setImageBitmap(imageBitmap)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
