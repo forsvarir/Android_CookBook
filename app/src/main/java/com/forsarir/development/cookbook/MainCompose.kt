5dbdbc1a5116c7718edcc01261aedae49a5f3cde
Mark Loveland-Armour <forsvarir@gmail.com>
2022-03-08 12:19:55 +0000

    Track Gesture with compose v1

--- app/src/main/java/com/forsarir/development/cookbook/MainCompose.kt
+++ app/src/main/java/com/forsarir/development/cookbook/MainCompose.kt
@@ -3,14 +3,28 @@ package com.forsarir.development.cookbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import com.forsarir.development.cookbook.ui.theme.CookBookTheme
+import kotlin.math.roundToInt
 
 class MainCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +36,43 @@ class MainCompose : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    ClickableSample()
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun ClickableSample() {
+    val gesture = remember { mutableStateOf("") }
+    val count = remember { mutableStateOf("") }
+    Box(modifier = Modifier.fillMaxSize()) {
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+
+        Box(
+            Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .background(Color.Blue)
+                .size(50.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        gesture.value = change.position.toString()
+                        change.consumeAllChanges()
+                        gesture.value += "->" + change.position.toString()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                        count.value = gesture.value + "==" + "(${dragAmount.x},${dragAmount.y})"
+                    }
+                }
+        )
+        Text(
+            text = count.value.toString()
+        )
+
     }
 }
 
