9501ceaa3ebad8812ac755f313dcb32e8109a466
Mark Loveland-Armour <forsvarir@gmail.com>
2022-03-08 15:29:38 +0000

    Added point tracking to gestures

--- app/src/main/java/com/forsarir/development/cookbook/MainCompose.kt
+++ app/src/main/java/com/forsarir/development/cookbook/MainCompose.kt
@@ -3,24 +3,17 @@ package com.forsarir.development.cookbook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import com.forsarir.development.cookbook.ui.theme.CookBookTheme
-import kotlin.math.roundToInt
 
 class MainCompose : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,42 +40,46 @@ fun ClickableSample() {
     val dragEnd = remember { mutableStateOf("") }
     val outputMessage = remember { mutableStateOf("") }
 
-    var startX by remember { mutableStateOf(0f) }
-    var startY by remember { mutableStateOf(0f) }
     var posX by remember { mutableStateOf(0f) }
     var posY by remember { mutableStateOf(0f) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
+    val points = mutableListOf<String>()
 
-    Box(modifier = Modifier.fillMaxSize()
+    Box(modifier = Modifier
+        .fillMaxSize()
         .pointerInput(Unit) {
             detectDragGestures(
                 onDragStart = {
                     dragStart.value = "FROM:(${it.x},${it.y})"
                     dragEnd.value = ""
-                    startX = it.x
-                    startY = it.y
                     posX = it.x
                     posY = it.y
 
+                    points.clear()
                     outputMessage.value = "${dragStart.value},${dragEnd.value}"
                 },
-                onDrag = { change, offset ->
+                onDrag = { _, offset ->
                     posX += offset.x
                     posY += offset.y
                     offsetX += offset.x
                     offsetY += offset.y
+                    points.add("($posX, $posY)")
                 },
                 onDragEnd = {
                     dragEnd.value = "TO:(${posX},${posY})"
 
-                    outputMessage.value = "${dragStart.value},${dragEnd.value}"
+                    val pointsMessage = points.reduce { acc, item ->
+                        "$acc,$item"
+                    }
+
+                    outputMessage.value = "${dragStart.value},${pointsMessage},${dragEnd.value}"
                 }
             )
         }
     ) {
         Text(
-            text = outputMessage.value.toString()
+            text = outputMessage.value
         )
 
     }
